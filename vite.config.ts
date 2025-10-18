5d7ea933e57c29c16522b72dceaf177435d5dbd0
tcbmem-png <tcbmem@gmail.com>
2025-10-18 14:17:54 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,26 @@
+logs
+*.log
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+pnpm-debug.log*
+lerna-debug.log*
+
+node_modules
+dist
+dist-ssr
+*.local
+
+.vscode/*
+!.vscode/extensions.json
+.idea
+.DS_Store
+*.suo
+*.ntvs*
+*.njsproj
+*.sln
+*.sw?
+.env
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
